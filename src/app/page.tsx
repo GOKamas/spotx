91ada1f31d599ea07962bde03106fdc29835a77b
SMDemas <comfysantorinitransfers@gmail.com>
2026-05-17 08:18:37 +0300

    fix: force dynamic rendering for azuracast api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import { Metadata } from 'next';
 import Hero from '@/components/Hero/Hero';
 import HomeBlogSection from '@/components/HomeBlogSection/HomeBlogSection';
